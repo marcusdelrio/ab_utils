b005ed51c223b88056b04c691f1a9d074b0d8575
Marcus <marcusdelrio@gmail.com>
2020-01-17 14:27:04 -0500

    qc-d build dataset, ci, graphing, and hypo test

--- ab.py
+++ ab.py
@@ -1,25 +1,21 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import binom,norm
+from scipy.stats import stats, binom,norm,zscore
 import seaborn as sns
 import math
 
-import scipy.stats as stats
 import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm, zscore
 
 
 def build_dataset(control_size=1000,variation_size=1000,control_cr=.4,variation_cr=.5):
     #create control
-    print(control_size)
     control_results = np.random.rand(control_size)<control_cr
     control_df = pd.DataFrame(control_results.astype(int),columns=['result'])
     control_df['group']='control'
 
     #create variation
-    print(variation_size)
     variation_results = np.random.rand(variation_size)<variation_cr
     variation_df = pd.DataFrame(variation_results.astype(int),columns=['result'])
     variation_df['group']='variation'
@@ -28,9 +24,13 @@ def build_dataset(control_size=1000,variation_size=1000,control_cr=.4,variation_
     return(df)
 
 def graph_distributions(df):
+    ###
+    #Plot the distributions of both Control & Variations from a an AB test
+    ###
 
-    control_p = df.group2y('group').mean().loc['control']
-    variation_p = df.group2y('group').mean().loc['variation']
+    probs =  df.groupby('group').mean()
+    control_p = probs.loc['control']
+    variation_p = probs.loc['variation']
 
     control = binom.rvs(size=1000,n=1000,p=control_p)
     variation = binom.rvs(size=1000,n=1000,p=variation_p)
@@ -44,40 +44,39 @@ def graph_distributions(df):
     return
 
 def ci(cr,n,alpha):
+    ###
+    #Get the Confidence Interval for a given proportion of a given sample size
+    ###
     z_alpha = norm.ppf(1-alpha/2)
     ci = z_alpha*math.sqrt((cr*(1-cr)/n))
     return (cr-ci,cr+ci)
 
-def hypothesis_test_prop(cr1,cr2,n):
-    ### cr2 is higher & Sample sizes are equivalent
-    ### one sided - double for 2 sided
+def hypothesis_test_prop(cr1,cr2,n,test='one'):
+    ###
+    # Sample sizes are equivalent
+    #TODO ADD two-sided test logic
+    ###
 
     se1 = math.sqrt((cr1*(1-cr1)/n))
     se2 = math.sqrt((cr2*(1-cr2)/n))
     se_diff = math.sqrt(se1**2+se2**2)
 
     z_score = ((cr2 - cr1) / se_diff)
-    p_value = norm.cdf(z_score)
 
     z = np.arange(-3,3,0.1)
     plt.plot(z,norm.pdf(z))
-    plt.fill_between(z[z<z_score],norm.pdf(z[z<z_score]))
+
+    if test!='two' and cr1<cr2:
+        plt.fill_between(z[z>z_score],norm.pdf(z[z>z_score]))
+        p_value = 1-norm.cdf(z_score)
+    elif test!='two' and cr1>cr2:
+        plt.fill_between(z[z<z_score],norm.pdf(z[z<z_score]))
+        p_value = norm.cdf(z_score)
+
+    plt.title("Hypothesis Test for cr1: %s & cr2: %s & %s samples"%(cr1,cr2,n))
+    plt.text(-3,.3,'p-value: {:6.3f}'.format(p_value))
     plt.show()
-    return(z_score,p_value)
 
-def hypothesis_test_cont(mean_a,mean_b,std_a,std_b,n_a,n_b):
-
-    #one sided - double for 2 sided
-
-    z_score = (mean_b-mean_a)/np.sqrt(std_b**2/n_b+std_a**2/n_a)
-    p_value = norm.sf(z_score)
-
-    print(p_value)
-
-    z = np.arange(-3,3,0.1)
-    plt.plot(z,norm.pdf(z))
-    plt.fill_between(z[z>z_score],norm.pdf(z[z>z_score]))
-    plt.show()
     return(z_score,p_value)
 
 def get_power(h0,h1,n,alpha=0.05,test='two'):
@@ -85,6 +84,7 @@ def get_power(h0,h1,n,alpha=0.05,test='two'):
     z_alpha = norm.ppf(1-alpha)
     se0 = np.sqrt(h0*(1-h0)/n) #standard error for proportion
     se1 = np.sqrt(h1*(1-h1)/n) #standard error for proportion
+
     upper_bound = h0+se0*z_alpha
     lower_bound = h0-se0*z_alpha
 
@@ -92,7 +92,7 @@ def get_power(h0,h1,n,alpha=0.05,test='two'):
     rv2 = norm(h1,se1)
 
 
-    x = np.linsp1ce(h0 - 5*se0, h0 + 5*se0, 100)
+    x = np.linspace(h0 - 5*se0, h0 + 5*se0, 100)
     plt.plot(x, rv.pdf(x))
     plt.plot(x, rv2.pdf(x))
     plt.fill_between(x[x<lower_bound],rv2.pdf(x[x<lower_bound]),color='r',alpha=.2)
